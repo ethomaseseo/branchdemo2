415f56203c1ce41048eafe870cc9938117ea5e54
Eric <eric.thomas@eseo.fr>
2025-01-13 14:53:48 +0100

    ajout de l'age

--- src/main/java/fr/eseo/Common.java
+++ src/main/java/fr/eseo/Common.java
@@ -4,9 +4,11 @@ public class Common {
 
     private String nom;
     private String prenom;
-
+    private int age;
     public Common(String nom, String prenom) {
         this.nom = nom;
         this.prenom = prenom;
+        age=0;
+
     }
 }
