56692fb31cf8ec21fd7419bd7b45533f82866b6a
Ada <bonivada@gmail.com>
2025-01-13 15:00:20 +0100

    ajout addresse

--- src/main/java/fr/eseo/Common.java
+++ src/main/java/fr/eseo/Common.java
@@ -4,9 +4,11 @@ public class Common {
 
     private String nom;
     private String prenom;
+    private String adresse;
 
-    public Common(String nom, String prenom) {
+    public Common(String nom, String prenom, String adresse) {
         this.nom = nom;
         this.prenom = prenom;
+        this.adresse = adresse;
     }
 }
